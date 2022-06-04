f7621705987902c07a4641828db1905cbd3d91db
diegoger222 <runyxp@gmail.com>
2022-06-04 13:50:39 +0200

    estadisticas

--- Assets/Scripts/Personaje/ExperienciaEstadisticas/Estadisticas.cs
+++ Assets/Scripts/Personaje/ExperienciaEstadisticas/Estadisticas.cs
@@ -1,9 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Estadisticas : MonoBehaviour
 {
+    public GameObject botonMenosDefensaFisica;
+    public GameObject botonMasDefensaFisica;
+    public Text textDefensaFisica;
+
+    public GameObject botonMenosDefensaMagica;
+    public GameObject botonMasDefensaMagica;
+    public Text textDefensaMagica;
+
+    public GameObject botonMenosDanyoFisico;
+    public GameObject botonMasDanyoFisico;
+    public Text textDanyoFisico;
+
+    public GameObject botonMenosDanyoMagico;
+    public GameObject botonMasDanyoMagico;
+    public Text textDanyoMagico;
+
+    public GameObject botonMenosMana;
+    public GameObject botonMasMana;
+    public Text textMana;
+
+    public GameObject botonMenosVitalidad;
+    public GameObject botonMasVitalidad;
+    public Text textVitalidad;
+
+    public GameObject botonMenosSuerte;
+    public GameObject botonMasSuerte;
+    public Text textSuerte;
+
+
+
 
     private float defensaFisicaBase = 0;
     private float defensaMagicaBase = 0;
@@ -22,6 +53,13 @@ public class Estadisticas : MonoBehaviour
     private float vitalidadVar;
     private float suerteVar;
 
+    private float defensaFisicaTotal;
+    private float defensaMagicaTotal;
+    private float danyoFisicoTotal;
+    private float danyoMagicoTotal;
+    private float manaTotal;
+    private float vitalidadTotal;
+    private float suerteTotal;
 
     private float p_defensaFisica;
     private float p_defensaMagica;
@@ -159,9 +197,107 @@ public class Estadisticas : MonoBehaviour
         nivel = a;
     }
 
+    public void MostrarBotones()
+    {
+        if(puntosDisponibles> 0)
+        {
+            botonMasDanyoFisico.SetActive(true);
+            botonMasDanyoMagico.SetActive(true);
+            botonMasDefensaFisica.SetActive(true);
+            botonMasDefensaMagica.SetActive(true);
+            botonMasMana.SetActive(true);
+            botonMasVitalidad.SetActive(true);
+            botonMasSuerte.SetActive(true);
+        }
+        else
+        {
+            botonMasDanyoFisico.SetActive(false);
+            botonMasDanyoMagico.SetActive(false);
+            botonMasDefensaFisica.SetActive(false);
+            botonMasDefensaMagica.SetActive(false);
+            botonMasMana.SetActive(false);
+            botonMasVitalidad.SetActive(false);
+            botonMasSuerte.SetActive(false);
+        }
+        // para poder desactivar y activar el boton de restar puntos
+        if (true)
+        {
+            if (p_danyoFisico > 0) 
+            {
+                botonMenosDanyoFisico.SetActive(true);
+            }
+            else
+            {
+                botonMenosDanyoFisico.SetActive(false);
+            }
+
+            if (p_danyoMagico > 0)
+            {
+                botonMenosDanyoMagico.SetActive(true);
+            }
+            else
+            {
+                botonMenosDanyoMagico.SetActive(false);
+            }
+            if (p_defensaFisica > 0)
+            {
+                botonMenosDefensaFisica.SetActive(true);
+            }
+            else
+            {
+                botonMenosDefensaFisica.SetActive(false);
+            }
+            if (p_defensaMagica > 0)
+            {
+                botonMenosDefensaMagica.SetActive(true);
+            }
+            else
+            {
+                botonMenosDefensaMagica.SetActive(false);
+            }
+            if (p_mana> 0)
+            {
+                botonMenosMana.SetActive(true);
+            }
+            else
+            {
+                botonMenosMana.SetActive(false);
+            }
+            if (p_suerte> 0)
+            {
+                botonMenosSuerte.SetActive(true);
+            }
+            else
+            {
+                botonMenosSuerte.SetActive(false);
+            }
+            if (p_vitalidad > 0)
+            {
+                botonMenosVitalidad.SetActive(true);
+            }
+            else
+            {
+                botonMenosVitalidad.SetActive(false);
+            }
+
+        }
+    }
+
     public void SubirPuntos() //esta funcion es llamada desde Experiencia cuando se sube de nivel para a�adir puntos;
     {
         puntosDisponibles++;
        // puntosMaxDisponibles++;
+    }
+
+    public void ActualizarEstadisticas()
+    {
+        
+        defensaFisicaTotal = defensaFisicaBase + defensaFisicaVar + p_defensaFisica*2;
+        defensaMagicaTotal = defensaMagicaBase + defensaMagicaVar + p_defensaMagica*2;
+        danyoFisicoTotal   = danyoFisicoBase + danyoFisicoVar + p_danyoFisico;
+        danyoMagicoTotal   = danyoMagicoBase + danyoMagicoVar + p_danyoMagico;
+        manaTotal           = manaBase  + manaVar + p_mana*120;
+        vitalidadTotal      = vitalidadBase + vitalidadVar +p_vitalidad * 10;
+        suerteTotal     = suerteBase +suerteVar + p_suerte;
     }
 }
