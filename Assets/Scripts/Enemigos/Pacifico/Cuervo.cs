07c4fa383ff7ac3bb01161e7d220d5a8bf3ef9be
diegoger222 <runyxp@gmail.com>
2022-07-05 13:12:39 +0200

    cuervo

---
+++ Assets/Scripts/Enemigos/Pacifico/Cuervo.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cuervo : MonoBehaviour
+{
+
+    [SerializeField] public Transform player;
+
+    [SerializeField] private float distancia;
+    public Vector3 puntoInicial;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        puntoInicial = transform.position;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distancia = Vector2.Distance(transform.position, player.position);
+        animator.SetFloat("Distancia", distancia);
+    }
+    public void Girar(Vector3 objetivo)
+    {
+        if( transform.position.x < objetivo.x)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+}
